eda917672b53f2809694f89c27bd952fd1cf830f
lilyVolo <lvolotchko@gmail.com>
2023-10-25 16:42:52 +0200

    drawer

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,9 +1,9 @@
-function Drawer() {
+function Drawer({onCloseCart}) {
     return (
         <div  className="overlay">
             <div className="drawer">
                 <h2 className="d-flex justify-between mb-30">
-                    Корзина <img className="cu-p" src="/img/btn-remove.svg" alt="Remove" />
+                    Корзина <img onClick={onCloseCart} className="cu-p" src="/img/btn-remove.svg" alt="Remove" />
                 </h2>
 
                 <div className="items">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 
 function Header({onClickCart}) {
-console.log(onClickCart)
+
   return (
     <header className='d-flex justify-between align-center p-40 '>
     <div className="d-flex align-center">
-    <img width={40} height={40} src="/img/logo.png" alt="" />
+    <img  width={40} height={40} src="/img/logo.png" alt="" />
     <div className='headerInfo'>
     <h3 className='text-uppercase'>React Sneakers</h3> 
     <p>Магазин лучших кросовок</p>
@@ -14,11 +14,14 @@ console.log(onClickCart)
     </div>
     <ul className='d-flex headerRight'>
       <li  className='mr-30'>
-        <img onClick={() => onClickCart} width={18} height={18} src="/img/user.svg" alt="" />
+        <img 
+        width={18} height={18} src="/img/user.svg" alt="" />
         <span>120 euro</span>
       </li>
       <li>
-        <img width={18} height={18} src="/img/cart.svg" alt="" />
+        <img 
+        onClick={onClickCart}
+         width={18} height={18} src="/img/cart.svg" alt="" />
       </li>
     </ul>
   </header>
