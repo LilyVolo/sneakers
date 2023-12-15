f7adb49e1712c2da68ec4296145d02d709ee6ae9
lilyVolo <lvolotchko@gmail.com>
2023-12-15 18:43:46 +0100

    +

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     proxy: {
 
       '/api': {
-        target: 'http://localhost:5005/api',
+        target: 'https://react-sneakers-node.onrender.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Remove "/gapi" prefix
       },
